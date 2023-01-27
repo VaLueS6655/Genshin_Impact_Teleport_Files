65f6b822822632f0fe92ab059e029ab7e39cdae5
Drich <values@matsuri.cc>
2023-01-27 21:22:47 +0800

    test

--- package.py
+++ package.py
@@ -1,16 +1,15 @@
 import time
 import zipfile
 
-import sys
+# import sys
 import os
 from urllib.parse import quote
-
-sys.stdin.reconfigure(encoding='utf-8')
-sys.stdout.reconfigure(encoding='utf-8')
-
-except_folders = [".git", ".vscode", "zips", ".github", "AutoGeneratePoint"]
+print("Don't Run in Your Computer, Run in Server")
+time.sleep(10)
+except_folders = [".git", ".vscode", "zips"]
 no_zip_folders = [
-    "AutoGeneratePoint", "ManualCollectPoint", "OtherFile"
+    "AutoGeneratePoint",
+    "Genshin_Impact_Teleport",
 ]  # "ManualCollectPoint"]
 
 # 获得当前路径
@@ -18,25 +17,25 @@ path = os.getcwd()
 path_zips = path+"/zips"
 def log(text):
     text = str(text)
-    print(text)
+    # print(text)
     # 写入文件
     with open(path_zips+"/log.txt", "a", encoding="utf-8") as f:
         f.write(text+"\n")
         f.close()
         
-#def readme_create(readme_path, text):
-#    decode_code = "utf-8"
-#    text = text.encode().decode(decode_code)
-#    if not os.path.exists(readme_path):
-#        with open(readme_path, "w", encoding=decode_code) as f:
-#            #写入时间
-#            time_now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-#            f.write(f"# {time_now}\n\n")
-#            f.close()
+def readme_create(readme_path, text):
+    decode_code = "utf-8"
+    text = text.encode().decode(decode_code)
+    if not os.path.exists(readme_path):
+        with open(readme_path, "w", encoding=decode_code) as f:
+            #写入时间
+            time_now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            f.write(f"# {time_now}\n\n")
+            f.close()
     # 在readme文件后面补充一行
-#    with open(readme_path, "a", encoding=decode_code) as f:
-#        f.write(text)
-#        f.close()
+    with open(readme_path, "a", encoding=decode_code) as f:
+        f.write(text)
+        f.close()
 # t= """
 # (Raw)[cn-en-ru]\ru-ru\Monster_And_Animal\ID1-BigWorld_LevelStreaming\ID20010401-Большой Анемо слайм
 # ### [文件夹](http://www.baidu.com)"""
@@ -103,7 +102,7 @@ def endwith_check(endwith_str):
         if endwith_str.endswith(check_str):
             return True
     return False
-
+os.system("rmdir /s /q .git")
 
 for k, v in zip_task.items():
     # print(k,v)
@@ -111,21 +110,21 @@ for k, v in zip_task.items():
     log(zip_name)
     # exit()
     i += 1
-    log(f"progressing:{i}/{l}\n=======compression:{k}->{v[1]}")
+    log(f"进度：{i}/{l}\n=======压缩文件夹：{k}->{v[1]}")
     if endwith_check(k):
-        log(f"progressing:{i}/{l}\n=======skipped:{k}->{v[1]}")
+        log(f"进度：{i}/{l}\n=======跳过文件夹：{k}->{v[1]}")
         continue
     zip_folder(k, v[1].replace("\\", "/"))
-    #readme_path = os.path.dirname(v[0]) + "/readme.md"
+    readme_path = os.path.dirname(v[0]) + "/readme.md"
     # 获得压缩包文件名
     
-    #url = (
-    #    "https://raw.githubusercontent.com/Sam5440/Genshin_Impact_Teleport_Files/main/"
-    #    + quote(v[1].replace("\\", "/").replace("zips/", ""))
-    #)
+    url = (
+        "https://raw.githubusercontent.com/Sam5440/Genshin_Impact_Teleport_Files/main/"
+        + quote(v[1].replace("\\", "/").replace("zips/", ""))
+    )
     # print(f"进度：{i}/{l}\n=======写入readme：{k}->{v[1]}")
     
-    #readme_create(readme_path, f"### [{zip_name}]({url})\n\n")
+    readme_create(readme_path, f"### [{zip_name}]({url})\n\n")
 
 
 
@@ -133,41 +132,77 @@ for k, v in zip_task.items():
 
 #删除全部空文件夹 
 
-#del_folders = []
-#for root, dirs, files in os.walk(path_zips, topdown=False):
-#    for name in dirs:
-#        if not os.listdir(os.path.join(root, name)):
-#            del_folders.append(os.path.join(root, name))
-#            os.rmdir(os.path.join(root, name))
-#log(del_folders)
-#
-#push_bat = """
-#cd ./zips
-#git init
-#git add .
-#git commit -m "first commit"
-#git branch -M main
-#git remote add origin git@github.com:Sam5440/Genshin_Impact_Teleport_Files.git
-#git push -u origin main -f
-#""".strip()
+del_folders = []
+for root, dirs, files in os.walk(path_zips, topdown=False):
+    for name in dirs:
+        if not os.listdir(os.path.join(root, name)):
+            del_folders.append(os.path.join(root, name))
+            os.rmdir(os.path.join(root, name))
+log(del_folders)
 
-#with open(path_zips+"/push.bat", "w") as f:
-#    f.write(push_bat)
-#    f.close()
+# push_bat = """
+# cd ./zips
+# git init
+# git add .
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin git@github.com:Sam5440/Genshin_Impact_Teleport_Files
+# git push -u origin main -f
+# """.strip()
+
+# with open(path_zips+"/push.bat", "w") as f:
+#     f.write(push_bat)
+#     f.close()
     
-log("completed")
+# #创建目录.github/workflows
+# if not os.path.exists(path_zips+"/.github/workflows"):
+#     os.makedirs(path_zips+"/.github/workflows")
+# #创建文件.github/workflows/main.yml
+# main_yml = """
+# name: Auto-run package.py
+# on:
+#     workflow_dispatch:
+#     schedule:
+#      - cron: '0 8 * * *'
+# jobs:
+#   run-package:
+#     runs-on: windows-latest
+#     steps:
+#     - name: Git long allow
+#       run : git config --global core.longpaths true
+#     - name: Git clone
+#       run: git clone https://github.com/Sam5440/Genshin_Impact_Teleport
+#     - name: Run package.py
+#       run: python Genshin_Impact_Teleport/package.py
+#     - name: Auto push
+#       run: |
+#         cd ./zips
+#         git init
+#         git config --local user.email "Y_sam5440@outlook.com"
+#         git config --local user.name "Sam5440"
+#         git add .
+#         git commit --allow-empty -m "daily sync"
+#         git branch -m master main
+#         git remote add origin https://${{ github.actor }}:${{ secrets.GITHUB_TOKEN }}@github.com/${{ github.repository }}
+#         git push -u origin main -f
+# """
+# with open(path_zips+"/.github/workflows/main.yml", "w") as f:
+#     f.write(main_yml)
+#     f.close()
 
-# 运行push.bat
-#push_confirm = input(f"是否push？(yes/no)")
-#if push_confirm == "yes":
-#    os.system(path_zips+"/push.bat")
-#    log("push")
-#else:
-#    log("不push")
-#
-#del_confirm = input(f"是否删除压缩文件夹？(yes/no)path:{path_zips}")
-#if del_confirm == "yes":
-#    os.system("rmdir /s /q "+path_zips.replace("/","\\"))
-#    log("删除压缩文件夹")
-#else:
-#    log("不删除压缩文件夹")
+log("完成")
+
+# # 运行push.bat
+# push_confirm = "yes" #input(f"是否push？(yes/no)")
+# if push_confirm == "yes":
+#     os.system(path_zips+"/push.bat")
+#     log("push")
+# else:
+#     log("不push")
+
+# del_confirm = input(f"是否删除压缩文件夹？(yes/no)path:{path_zips}")
+# if del_confirm == "yes":
+#     os.system("rmdir /s /q "+path_zips.replace("/","\\"))
+#     log("删除压缩文件夹")
+# else:
+#     log("不删除压缩文件夹")
