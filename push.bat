052589af8c8ba2420213a9dc5d5d2fc792bc85b8
Drich <values@matsuri.cc>
2023-01-29 00:51:06 +0800

    UBUNTU

--- push.bat
+++
@@ -1,7 +0,0 @@
-cd ./zips
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:Sam5440/Genshin_Impact_Teleport_Files.git
-git push -u origin main -f
